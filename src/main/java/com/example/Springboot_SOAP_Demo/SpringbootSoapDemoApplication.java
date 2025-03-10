b1a8c5127c43d0903c4e60b866ac4447b231e61a
jaychand <jaychand.kumar@cloud4things.com>
2025-03-10 17:56:59 +0530

    refrence to run project and study project added

--- src/main/java/com/example/Springboot_SOAP_Demo/SpringbootSoapDemoApplication.java
+++ src/main/java/com/example/Springboot_SOAP_Demo/SpringbootSoapDemoApplication.java
@@ -26,3 +26,29 @@ youtube link to study -> https://youtu.be/3I_-oRLTrHU?si=_fYVCembjPdTx4j9
 Install SOAP-UI and give link of employees.wsdl for web-service request in xml,
 	update request xml file and send request.
  */
+
+
+
+
+
+
+/*
+
+
+-----------------------------------------------------
+SOAP web service in spring boot :-
+-----------------------------------------------------
+
+
+----------------------
+
+url to load - wsdl file  => http://localhost:9090/soap_Demo/employees.wsdl
+Project Name -> Springboot-Soap-Demo-CRUP_OP
+
+youtube link to study -> https://youtu.be/3I_-oRLTrHU?si=_fYVCembjPdTx4j9
+---------------------
+link -> https://youtu.be/3I_-oRLTrHU?si=_fYVCembjPdTx4j9
+https://youtu.be/mr_2-AWYCoc?si=Zm8atTiO5WHPl1Qv
+https://youtu.be/_j-CqV9fbWw?si=pcn-pp1lAoKdg24P
+
+ */
